b0b78a2f3fb1dc9e759b0f21e13a459cb148a0fa
20150300 <kljj0650@naver.com>
2020-06-25 05:04:18 +0900

    오타 수정 및 고유번호 입력 추가

--- MyOtpClient.js
+++ MyOtpClient.js
@@ -1,13 +1,18 @@
 const crypto = require('crypto')
-const serierNumber = 123646434
 
-const genOTP = (sec) => {
+const genOTP = (sec, seriarNumber) => {
     var hex = crypto.createHash('sha256').update(sec.toString()).digest('hex')
-    return parseInt(hex, 16) / serierNumber % 10000000
+    return parseInt(hex, 16) / seriarNumber % 10000000
 }
 
 const main = () => {
-    console.log(genOTP(Math.floor((new Date()).getTime() / 1000)))
+    if (process.argv.length != 3) {
+        console.log("$MyOptClient [Serial]")
+        return
+    }
+    var seriarNumber = process.argv[2]
+
+    console.log(genOTP(Math.floor((new Date()).getTime() / 1000), seriarNumber))
 }
 
 main()
--- MyOtpServer.js
+++ MyOtpServer.js
@@ -1,24 +1,21 @@
 const crypto = require('crypto')
 
-
 var otp = new Array(30)
-const serierNumber = 123646434
 
-
-const genOTP = (sec) => {
+const genOTP = (sec, seriarNumber) => {
     var hex = crypto.createHash('sha256').update(sec.toString()).digest('hex')
-    return parseInt(hex, 16) / serierNumber % 10000000
+    return parseInt(hex, 16) / seriarNumber % 10000000
 }
 
-const makeList30s = (curTime) => {
+const makeList30s = (curTime, seriarNumber) => {
     var start = curTime - 30
     for (var i = 0; i < 30; ++i) {
-        otp[i] = genOTP(start + i)
+        otp[i] = genOTP(start + i, seriarNumber)
     }
 }
 
-const checkOTP = (clientOTP) => {
-    makeList30s(Math.floor((new Date()).getTime() / 1000)) 
+const checkOTP = (clientOTP, seriarNumber) => {
+    makeList30s(Math.floor((new Date()).getTime() / 1000), seriarNumber) 
     for (var i = 0; i < 30; ++i) {
         if (otp[i] == clientOTP) {
             return true
@@ -28,8 +25,13 @@ const checkOTP = (clientOTP) => {
 }
 
 const main = () => {
+    if (process.argv.length != 4) {
+        console.log("$MyOptServer [OTP] [Serial]")
+        return
+    }
     var clientOTP = process.argv[2]
-    console.log(checkOTP(clientOTP))    
+    var seriarNumber = process.argv[3]
+    console.log(checkOTP(clientOTP, seriarNumber))    
 }
 
 main()
